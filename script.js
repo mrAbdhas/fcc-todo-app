4f71cf4a543e148fd1290881f3e78edd4b5bd463
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-02-27 00:44:19 +0100

    step10-26 Implement, get input values then save to array and display to UI and clear input fields

--- script.js
+++ script.js
@@ -24,6 +24,25 @@ let currentTask = {
 
 };
 
+
+//function to clear input fields, after creating a task.
+const reset = () => {
+    //first step, set the values for all 3 inputs fields to an empty string. 
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
+
+    //Next, toggle the hidden class on taksform.
+    taskForm.classList.toggle("hidden"); 
+    
+    /*and, Set currentTask object to empty object, 
+    so it can be filled with the task the user might have added.*/  
+    currentTask = {
+
+    };
+
+}
+
 // Event listener for the "Add new Task" button to toggle the visibility of the form modal.
 openTaskFormBtn.addEventListener("click", () => {
     // Toggles the "hidden" class on the taskForm element to show the form modal.
@@ -50,3 +69,49 @@ discardBtn.addEventListener("click", () => {
     // Toggles the "hidden" class on the taskForm element to hide the form modal.
     taskForm.classList.toggle("hidden");
 })
+
+//Event listener for submit button "Add Task", submit inputs to taskData array and display to UI.
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault(); //Prevent from refresing the page after submitting the form.
+    //Determine whether the task exist in taskData array
+    /* findIndex array method call on taskData finds and returns the index of the first
+    element that meets the criteria specified by a provied testing funtion, if not, 
+    it returns -1 */
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id)
+    //Retrive values from input fields and store them in a taskObj object, give each task a unique id.
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join('-')}-${Date.now()}`, //unique id to each task, titleInput.value in lowerCase hyphenated, with DateNow = number of milisecounds since 01/01/1970.
+        title: `${titleInput.value}`,
+        date: `${dateInput.value}`,
+        description: `${descriptionInput.value}`,
+        
+    };
+    //Add obtained input values in taskObj to taskData, if the, if condition is met.
+    if(dataArrIndex === -1) { // the condition, check if dataArrIndex is stricly equals to -1, meaning no matching id.
+        //Add taskObj object to the begining of taskData array.
+        taskData.unshift(taskObj);
+    }
+        //Display the task by looping throught it
+        // Use forEach method on taskData, then destructure id, title, date, description as the parameters 
+        taskData.forEach(({id, title, date, description}) => {
+            /*use addition assigment += operator to append HTML content in template literals
+            to taskContainer element*/ 
+            /*To allow for task management, 
+            include both a delete and an edit button for each task.*/
+            (tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+                <p><strong>Title:</strong>${title}</p>
+                <p><strong>Date:</strong>${date}</p>
+                <p><strong>Description:</strong>${description}</p>
+
+                <button type="button" class="btn" >Edit</button>
+                <button type="button" class="btn">Delete</button>
+            </div>
+            
+            `)
+        }
+        );      
+         /*call reset fucntion to clear input fields
+         and hide form modal, so user can see the added task.*/
+         reset(); 
+});
